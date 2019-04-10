91ff3a883d3c585688c368333851d7edfd2e45a5
Casey Randall <42552122+gospel02@users.noreply.github.com>
2019-04-10 10:24:07 -0600

    Products Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ export default class Navbar extends Component {
                 <Link to='/cart' className='ml-auto'>
                     <ButtonContainer>
                         <span className='mr-2'>
-                            <i className='fas fa-cart-plus' /> 
+                            <i className='fas fa-cart-arrow-down' /> 
                         </span>
                             my cart
                     </ButtonContainer>
